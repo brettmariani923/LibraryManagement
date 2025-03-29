ef94fb23f7d1f209fb406899ef1c4c48529b42db
Daniel Aguirre <uhyeay24@gmail.com>
2025-03-28 22:18:42 -0500

    Added IDbConnectionFactory, SqlConnectionFactory, and tests.

---
+++ LibraryManagement.Data.Tests/ImplementationTests/SqlConnectionFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using LibraryManagement.Data.Implementation;
+
+namespace LibraryManagement.Data.Tests.ImplementationTests
+{
+    public class SqlConnectionFactoryTests
+    {
+        [Theory]
+        [InlineData("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;")]
+        [InlineData("Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;")]
+        public void SqlConnectionFactory_Given_ConnectionString_IsValid_NewConnection_ShouldReturn_SqlConnection(string connectionString)
+        {
+            var connectionFactory = new SqlConnectionFactory(connectionString);
+
+            var connection = connectionFactory.NewConnection();
+
+            Assert.NotNull(connection);
+            Assert.Equal(connection.ConnectionString, connectionString);
+        }
+
+        [Fact]
+        public void SqlConnectionFactory_Given_ConnectionString_IsNotValid_NewConnection_ShouldThrow_ArgumentException()
+        {
+            var invalidConnectionString = "Not A Real Connection String";
+
+            var connectionFactory = new SqlConnectionFactory(invalidConnectionString);
+
+            Assert.Throws<ArgumentException>(() => connectionFactory.NewConnection());
+        }
+    }
+}

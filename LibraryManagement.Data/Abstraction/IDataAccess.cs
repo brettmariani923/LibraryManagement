561a9105489a8d7c0dd2820f6a64929ce16181a0
Daniel Aguirre <uhyeay24@gmail.com>
2025-03-29 12:06:19 -0500

    - Created IDataRequest    - This base interface is used for SQL Requests (Execute, Query, QueryFirstOrDefault).    - IDataExecute, IDataFetch, and IDataFetchList implement the IDataRequest. Each of these are used for a specific type of operation. - Created IDataAccess    - This defines the contract for receiving IDataRequests to send to the database. This is like a 'Wrapper' around our integration with 'Dapper'.    - We have a separate method for Execute, Fetch, and FetchList. Each of these will receive an IDataExecute, IDataFetch, or IDataFetchList. - Created basic implementation for DataAccess. We will revisit this when we add logging. - Created a couple Example DataRequestObjects.    - ExampleFetchById shows an example that uses parameters.    - ExampleFetchAll shows an example that uses no parameters. - Created an example Data Transfer Object    - There's more details on what a 'DTO' is commented on the ExampleFetch_DTO class. - Created DataTest base class to use for any tests that will send an IDataRequest to the database.    - You can see an example of using it with more details on the ExampleFetchByIdTests class.    - This class references another class 'Hidden.cs' which is added to the gitignore. This means you will need to manually add the class to your local project. More details are included in the DataTest class.

---
+++ LibraryManagement.Data/Abstraction/IDataAccess.cs
@@ -0,0 +1,32 @@
+﻿namespace LibraryManagement.Data.Abstraction
+{
+    /// <summary>
+    /// This interface will abstract out the integration with Dapper. 
+    /// Each method will receive an IDataRequest (Execute, Fetch, or FetchList), which will define the Sql and Parameters to pass to Dapper.
+    /// </summary>
+    public interface IDataAccess
+    {
+        /// <summary>
+        /// Executes the DataRequest using GetSql() and GetParameters() from the IDataExecute request. 
+        /// </summary>
+        /// <param name="request">The type of DataRequest to Execute</param>
+        /// <returns>The number of rows impacted</returns>
+        public Task<int> ExecuteAsync(IDataExecute request);
+
+        /// <summary>
+        /// Fetch the FirstOrDefault result from the query provided by the DataRequest using GetSql() and GetParameters() from the IDataFetch request.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of DTO being returned by the SQL Query</typeparam>
+        /// <param name="request">The type of DataRequest to Fetch</param>
+        /// <returns>Returns an object of the type TResponse associated with the IDataFetch.</returns>
+        public Task<TResponse?> FetchAsync<TResponse>(IDataFetch<TResponse> request);
+
+        /// <summary>
+        /// Fetch the collection of results from the query provided by the DataRequest using GetSql() and GetParameters() from the IDataFetchList request.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of DTO that a collection of is being returned by the SQL Query</typeparam>
+        /// <param name="request">The type of DataRequest to Fetch</param>
+        /// <returns>Returns an IEnumerable of objects of the type TResponse associated with the IDataFetch.</returns>
+        public Task<IEnumerable<TResponse>> FetchListAsync<TResponse>(IDataFetchList<TResponse> request);
+    }
+}

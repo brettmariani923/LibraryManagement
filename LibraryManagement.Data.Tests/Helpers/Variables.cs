6b5f2a0fa72076b470460a738c4513e80ef82904
Brett Mariani <brettmariani923@gmail.com>
2025-04-17 06:38:13 -0500

    Moved TestVariables class up to Data.Tests, renamed it to Variables

--- LibraryManagement.Data.Tests/Helpers/Variables.cs
+++ LibraryManagement.Data.Tests/Helpers/Variables.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryManagement.Domain.Constants
+namespace LibraryManagement.Data.Tests.Helpers
 {
-    public class Variables
+    internal class Variables
     {
         public const string Title = "The Republic";
         public const string Summary = "A timeless classic and a total heart puncher sure to make you weep tears of joy";
         public const string PublishedYear = "1000 AD";
     }
+}
 }
